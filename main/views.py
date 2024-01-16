cac3141d039d2542392a15b1bcafa26d14f5a94d
Rozievich <blackcoder459@outlook.com>
2024-01-16 23:15:55 +0500

    Update from code

--- main/views.py
+++ main/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 
 def home(request):
-    return HttpResponse("<h1 style='text-align: center; color: blue;'>Hello World</h1>")
+    return render(request, "index.html")
