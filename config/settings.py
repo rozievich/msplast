e3c556b7e1616fc4f6fc088ddda876b953833ef5
Rozievich <blackcoder459@outlook.com>
2024-01-16 22:52:24 +0500

    requirements add to project

--- config/settings.py
+++ config/settings.py
@@ -85,6 +85,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 JAZZMIN_SETTINGS = {
