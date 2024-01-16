22be1fad36ef89eeebb5e4702512a49cdf90e78f
Rozievich <blackcoder459@outlook.com>
2024-01-16 23:27:39 +0500

    Update in code

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+from .settings import DEBUG
 
 urlpatterns = [
     path('site/admin/', admin.site.urls),
     path('', include('main.urls'))
 ]
+if DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)\
+                + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
